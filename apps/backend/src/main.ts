1934a539500c34eb9511632b618c6e110eebab21
fsvila <fsvila@hotmail.com>
2025-05-21 10:40:48 -0300

    feat: api de planilla de evaluacion

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,6 +8,9 @@ import AuthRouter from './app/auth/auth.routes';
 import { UsersRouter } from './app/users/user.controller';
 import agenterouter from './app/agentes/agentes.router';
 
+import planillaEDEvaluacionRouter from './app/PlanillaEDEvaluacion/PlanillaEDEvaluacion.routers';
+
+
 require('dotenv').config();
 console.log('🔍 JWT_SECRET leído desde .env:', process.env.JWT_SECRET);
 
@@ -24,6 +27,8 @@ application.add({ path: '/api', router: ServiciosRouter })
 application.add({ path: '/api', router: PlanillaEDRouter })
 application.add({ path: '/api', router: EdCategoriaItems })
 application.add({ path: '/api', router: agenterouter })
+application.add({ path: '/api', router: planillaEDEvaluacionRouter })
+application.add({ path: '/api/planilla-ed', router: planillaEDEvaluacionRouter });
 
 // ruta para la autenticación (login)
 application.add({ path: '/api', router: AuthRouter });
